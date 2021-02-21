079b6ba109d88b55cc31dceb1dbdb9a01757b363
Muujii Tsend <munkhdelger27@gmail.com>
2021-02-21 00:32:50 -0800

    iterative solution added

--- Binary Tree/Leetcode144. Binary Tree Preorder Traversal.js
+++ Binary Tree/Leetcode144. Binary Tree Preorder Traversal.js
@@ -21,21 +21,52 @@ The number of nodes in the tree is in the range [0, 100].
 
  */
 
-var preorderTraversal = function solution(root) {
-    
+
+const preorderTraversal = (root, res = []) => {
+    if (!root) return [];
+    res.push(root.val);
+    if (root.left) preorderTraversal(root.left, res);
+    if (root.right) preorderTraversal(root.right, res);
+    return res;
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+}
+
+/*
+const preorderTraversal = (root) => {
+    let result = [];
+    let stack = [root];
+    while(stack.length > 0){
+        let cur = stack.pop();
+        if (cur !== null){
+            result.push(cur.val);
+            if (cur.right) stack.push(cur.right);
+            if (cur.left) stack.push(cur.left);
+        }
+    }
+    return result;
+}
+*/
+
+/* Recursive, called another function Time Complexity O(n), Space O(n)
+
+const preorderTraversal = root => {  
     let res = [];
     traverse(root)
     return res;
     
-    function traverse(node){
-        if (!node) return [];
+   function traverse(node){
+        if (!node) return;
         res.push(node.val);
         traverse(node.left);
         traverse(node.right);
     }
 };
 
-/*
+*/
+
+/* Recursive solution Time Complexity is O(n), Space Complexity is O(n)
+
 var preorderTraversal = function(root) {
     let result = [];
     
