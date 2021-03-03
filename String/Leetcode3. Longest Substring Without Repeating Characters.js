85be8f455bba6400b15218b79e0cb26b82c41256
Muujii Tsend <munkhdelger27@gmail.com>
2021-03-03 00:04:45 -0800

    Longest Substring Without Repeating Characters

---
+++ String/Leetcode3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ 
+ 3. Longest Substring Without Repeating Characters
+ Given a string s, find the length of the longest substring without repeating characters.
+ Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+ 
+ */
+
+
+
+var lengthOfLongestSubstring = function(s) {
+    
+    let alphaCol = {};
+    let maxLen = 0;
+    for (let i=0; i < s.length; i++) {
+         //  console.log(alphaCol, i);
+        if (alphaCol.hasOwnProperty(s[i])){
+            if (maxLen < Object.keys(alphaCol).length){
+                maxLen = Object.keys(alphaCol).length;
+            }
+            alphaCol = {};
+            i = alphaCol[s[i]];
+        } else {
+                alphaCol[s[i]] = i;
+        }
+    }
+ 
+    return maxLen < Object.keys(alphaCol).length ? Object.keys(alphaCol).length : maxLen;    
+};
