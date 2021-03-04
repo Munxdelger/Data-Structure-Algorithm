107376e418f40af78056cccf87d4668ec0d9eff6
Muujii Tsend <munkhdelger27@gmail.com>
2021-03-03 20:53:07 -0800

    longest substring

--- String/Leetcode3. Longest Substring Without Repeating Characters.js
+++ String/Leetcode3. Longest Substring Without Repeating Characters.js
@@ -11,8 +11,29 @@ s consists of English letters, digits, symbols and spaces.
  
  */
 
+const lengthOfLongestSubstring = (s) => {    
+    let alphas = new Map();
+    let maxLen = 0;  
+    let p1 = 0;
+    
+    for (let i=0; i < s.length; i++) {
+        if (alphas.has(s[i])){
+            maxLen = Math.max(maxLen, i-p1);  
+            for (let j = p1; j < alphas.get(s[i]); j++) {
+                alphas.delete(s[j]);
+            }
+            p1 = alphas.get(s[i]) + 1;
+            alphas.set(s[i], i);    
+        } else {
+            alphas.set(s[i], i);
+        }
+    }    
+    return Math.max(maxLen, alphas.size);
+}
 
 
+
+/*
 var lengthOfLongestSubstring = function(s) {
     
     let alphaCol = {};
@@ -31,3 +52,5 @@ var lengthOfLongestSubstring = function(s) {
  
     return maxLen < Object.keys(alphaCol).length ? Object.keys(alphaCol).length : maxLen;    
 };
+
+*/
